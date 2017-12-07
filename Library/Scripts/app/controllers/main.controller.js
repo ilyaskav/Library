a9a1740c48c28dcd9bebc6575d38720f2d9a7190
ilyaskav <ilya.skav@gmail.com>
2017-12-07 21:56:10 +0200

    minor changes

--- Library/Scripts/app/controllers/main.controller.js
+++ Library/Scripts/app/controllers/main.controller.js
@@ -2,9 +2,25 @@
 
 app.controller('MainController', function ($scope, $http) {
     $scope.book = {};
+    $scope.books = [];
 
     $scope.showAddBookSection = function () {
         $scope.action = 'add';
+    }; 
+
+    $scope.showEditBookSection = function () {
+        $scope.action = 'edit';
+    };
+
+    $scope.redrawBooksTable = function () {
+        $http.get('api/book')
+            .then(function (res) {
+                $scope.books = res;
+                console.log(res);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
     };
 
     $scope.createBook = function (form) {
